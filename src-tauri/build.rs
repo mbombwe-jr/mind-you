5cdb913785c53a244d77479ff67fe24f256d09f1
mbombwe-jr <zoofam26@gmail.com>
2025-11-16 14:21:03 +0300

    add: push to release workflow

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -2,8 +2,9 @@
 fn main() {
     println!("cargo:rustc-link-arg=/STACK:33554432");
     tauri_build::build();
-    //   set TAURI_SIGNING_PRIVATE_KEY=H:\Github\rust-desktop-2025-tabletop\~\key\tabletopv1.key
-    //   set RUSTFLAGS=-C link-args=/STACK:33554432 && cargo run --release
 }
 
-
+#[cfg(not(windows))]
+fn main() {
+    tauri_build::build();
+}
